85caa006feb257fff653289d27479736a75b1057
Victor Domecq <vdomecq@ensc.fr>
2019-10-26 14:41:23 +0200

    Add Uuid type

--- flow-typed/custom-types.js
+++ flow-typed/custom-types.js
@@ -1,54 +1,94 @@
+declare opaque type Uuid: string
+
+declare type Response<T> = Success<T> | Failed
+
+declare type Success<T> = {|
+  fulfilled: true,
+  value: T
+|}
+
+declare type Failed = {|
+  rejected: true,
+  reason: {
+    cause: {
+      kind: any
+    }
+  }
+|}
+
 declare type Team = {
-  id: string,
+  id: Uuid,
   name: string,
-  rules: Rule[],
+  rules: Rule[]
 }
 
 declare type Rule = {
-  id: string,
+  id: Uuid,
   name: string,
   description: string,
-  category: 'TRAINING_DAY' | 'GAME_DAY',
-  kind: BasicKind | MultiplicationKind,
+  category: RuleCategory,
+  kind: BasicKind | MultiplicationKind | TimeMultiplicationKind | RegularIntervalsKind
 }
+
+declare type RuleCategory = 'TRAINING_DAY' | 'GAME_DAY'
 
 declare type BasicKind = {
   type: 'BASIC',
-  price: number,
+  price: number
 }
 
 declare type MultiplicationKind = {
   type: 'MULTIPLICATION',
-  price_to_multiply: number,
+  price_to_multiply: number
 }
 
 declare type TimeMultiplicationKind = {
   type: 'TIME_MULTIPLICATION',
   price_per_time_unit: number,
-  time_unit: TimeUnit,
+  time_unit: TimeUnit
 }
 
 declare type RegularIntervalsKind = {
   type: 'REGULAR_INTERVALS',
   price: number,
   interval_in_time_unit: number,
-  time_unit: TimeUnit,
+  time_unit: TimeUnit
 }
 
-declare type TimeUnit =
-  | 'SECOND'
-  | 'MINUTE'
-  | 'HOUR'
-  | 'DAY'
-  | 'WEEK'
-  | 'MONTH'
-  | 'YEAR'
+declare type TimeUnit = 'SECOND' | 'MINUTE' | 'HOUR' | 'DAY' | 'WEEK' | 'MONTH' | 'YEAR'
 
 declare type User = {
-  id: string,
-  team_id: string,
+  id: Uuid,
+  team_id: Uuid,
   lastname: string,
   firstname: string,
   nickname: ?string,
-  email: ?string,
+  email: ?string
+}
+
+declare type CreateSanction = {
+  team_id: Uuid,
+  user_id?: Uuid,
+  sanction_info?: SanctionInfo
+}
+
+declare type SanctionInfo = {
+  associated_rule: ?Uuid,
+  sanction_data: SanctionData
+}
+
+declare type SanctionData = BasicSanctionData | MultiplicationSanctionData | TimeMultiplicationSanctionData
+
+declare type BasicSanctionData = {
+  type: 'BASIC'
+}
+
+declare type MultiplicationSanctionData = {
+  type: 'MULTIPLICATION',
+  multiple: number
+}
+
+declare type TimeMultiplicationSanctionData = {
+  type: 'TIME_MULTIPLICATION',
+  times_unit: number
 }
