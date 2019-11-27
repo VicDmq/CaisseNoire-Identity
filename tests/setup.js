3122c10dfbab8b09c6d18f7d22e0149adb8b90a6
Victor Domecq <vdomecq@ensc.fr>
2019-11-27 19:27:00 +0100

    Remove old config and init tests for Login

--- tests/setup.js
+++ tests/setup.js
@@ -1,16 +1,5 @@
-// setup file
 import 'regenerator-runtime/runtime'
 import '@testing-library/jest-dom/extend-expect'
 import { configure } from '@testing-library/dom'
 
 configure({ testIdAttribute: 'test-id' })
-
-// Override window.fetch which is undefined otherwise
-global.fetch = () => {
-  throw 'DO NOT CALL fetch() FROM TESTS'
-}
-
-// Override window.Request which is undefined otherwise
-global.Request = () => {
-  throw 'DO NOT CALL Request FROM TESTS'
-}
