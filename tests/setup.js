0ffb70fd1007c74ac78b838c5225150dd01a01e4
Victor Domecq <vdomecq@ensc.fr>
2019-11-27 18:21:14 +0100

    Change data-testid to test-id

--- tests/setup.js
+++ tests/setup.js
@@ -1,6 +1,9 @@
 // setup file
 import 'regenerator-runtime/runtime'
 import '@testing-library/jest-dom/extend-expect'
+import { configure } from '@testing-library/dom'
+
+configure({ testIdAttribute: 'test-id' })
 
 // Override window.fetch which is undefined otherwise
 global.fetch = () => {
