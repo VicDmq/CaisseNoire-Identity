459ce67d65571cc1e28fa8b73ab703e3cac184ba
Victor Domecq <vdomecq@ensc.fr>
2020-01-06 11:11:13 +0100

    Write test for table feature

--- src/sanctions/SanctionTable/SanctionTable.js
+++ src/sanctions/SanctionTable/SanctionTable.js
@@ -21,16 +21,31 @@ const columns = [
     title: 'Cotisations',
     dataIndex: 'cotisations',
     key: 'cotisations',
+    onCell: () => {
+      return {
+        'test-id': 'cotisation',
+      };
+    },
   },
   {
     title: 'Sanctions',
     dataIndex: 'sanctions',
     key: 'sanctions',
+    onCell: () => {
+      return {
+        'test-id': 'sanction',
+      };
+    },
   },
   {
     title: 'Total',
     dataIndex: 'total',
     key: 'total',
+    onCell: () => {
+      return {
+        'test-id': 'total',
+      };
+    },
   },
 ];
 
