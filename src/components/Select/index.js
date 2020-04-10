2b2dfd580dc787e64d2a156be6c5e4b0bddd30a9
Victor Domecq <vdomecq@ensc.fr>
2020-04-10 17:55:44 +0200

    Fix flow 2

--- src/components/Select/index.js
+++ src/components/Select/index.js
@@ -1,4 +1,4 @@
 // @flow
 export { default as MultipleSelect } from './MultipleSelect';
 export { default as SingleSelect } from './SingleSelect';
-export type { OptGroupProps, CommonSelectProps } from './CommonSelect';
+export type { OptGroupProps, CommonSelectProps, OptionProps } from './CommonSelect';
